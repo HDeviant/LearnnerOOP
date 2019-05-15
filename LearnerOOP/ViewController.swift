3620186bc89c1196c5cc7c47cf76e7087fff3a4b
Vallen <devianto.vallen@gmail.com>
2019-05-15 15:44:47 +0700

    commit

--- LearnerOOP/ViewController.swift
+++ LearnerOOP/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var learnerGenderModel: UILabel!
+    @IBOutlet weak var learnerAgeModel: UILabel!
+    @IBOutlet weak var learnerNameModel: UILabel!
+    var learnerInstance : LearnerModel?
+    var facilitatorInstance : FacilitatorModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        learnerInstance = LearnerModel(nameLearner: "Vallen", ageLearner: 21, genderLearner: "Male", imageProfileLearner: "Photo1")
+        facilitatorInstance = FacilitatorModel(facilName: "ASD", facilAge: 19, facilGender: "MALE", facilImageProfile: "Photo2", facilPerk: "")
+        updateUI()
+    }
+    
+    
+    func updateUI() {
+        if let instance = facilitatorInstance {
+            learnerNameModel.text = instance.name
+            learnerAgeModel.text = "\(instance.age)"
+            learnerGenderModel.text = instance.gender
+        
+        }
+       
     }
 
+    @IBAction func increaseAgeButtonClicked(_ sender: UIButton) {
+        if let instance = facilitatorInstance {
+            instance.increaseAge()
+            instance.coachMentee()
+            updateUI()
+        }
+    }
+    
+    
 
-    
-    
-    
-    
     
     
     
